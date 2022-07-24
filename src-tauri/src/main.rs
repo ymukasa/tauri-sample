0449919af404b1277f84ed10f8231d06086cf3ec
ymukasa <ball.head.21@gmail.com>
2022-07-24 13:27:57 +0000

    ざっとサンプルの内容を実装しました。

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,80 @@
+#![cfg_attr(
+  all(not(debug_assertions), target_os = "windows"),
+  windows_subsystem = "windows"
+)]
+
+use tauri::Manager;
+
+fn main() {
+  tauri::Builder::default() 
+    .setup(|app| {
+        let id = app.listen_global("front-to-back", |event| {
+            println!(
+                "got front-to-back with payload {:?}",
+                event.payload().unwrap()
+            )
+        });
+        let app_handle = app.app_handle();
+        std::thread::spawn(move || loop {
+            app_handle
+                .emit_all("back-to-front", "ping frontend".to_string())
+                .unwrap();
+            std::thread::sleep(std::time::Duration::from_secs(1))
+        });
+        Ok(())
+    })
+    .invoke_handler(tauri::generate_handler![
+      simple_command,
+      command_with_message,
+      command_with_object,
+      command_with_error,
+      async_command
+    ])
+    .run(tauri::generate_context!())
+    .expect("error while running tauri application");
+}
+
+#[tauri::command]
+fn simple_command() {
+    println!("I was invoked from JS!");
+}
+
+#[tauri::command]
+fn command_with_message(message: String) -> String {
+    format!("hello {}", message)
+}
+
+use serde::{ Serialize, Deserialize };
+
+#[derive(Debug, Serialize, Deserialize)]
+struct MyMessage {
+    field_str: String,
+    field_u32: u32,
+}
+
+#[tauri::command]
+fn command_with_object(message: MyMessage) -> MyMessage {
+    let MyMessage {
+        field_str,
+        field_u32,
+    } = message;
+
+    MyMessage {
+        field_str: format!("hello {}", field_str),
+        field_u32: field_u32 + 1,
+    }
+}
+
+#[tauri::command]
+fn command_with_error(arg: u32) -> Result<String, String> {
+    if arg % 2 == 0 {
+        Ok(format!("even value {}", arg))
+    } else {
+        Err(format!("odd value {}", arg))
+    }
+}
+
+#[tauri::command]
+async fn async_command(arg: u32) -> String {
+    "hello".into()
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,82 @@
-import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { invoke } from '@tauri-apps/api/tauri'
+import { open } from '@tauri-apps/api/dialog'
+import { emit } from '@tauri-apps/api/event'
+import { useEffect } from 'react';
+import { listen } from '@tauri-apps/api/event'
 
 function App() {
+  function executeCommands() {
+
+    invoke('simple_command')
+
+    invoke(
+      'command_with_message',
+      { 
+        message: 'some message' 
+      }
+    ).then(message => {
+      console.log('command_with_messge', message)
+    })
+
+    invoke('command_with_object', { message: { field_str: 'some message', field_u32: 12 }}).then(message => {
+      console.log('command_with_object', message)
+    })
+
+    for (let arg of [1, 2]) {
+      invoke('command_with_error', { arg }).then(message => {
+        console.log('command_with_error', message)
+      }).catch(message => {
+        console.error('command_with_error', message)
+      })
+    }
+
+    invoke('async_command', { arg: 14 }).then(message => {
+      console.log('async_command', message)
+    })
+
+  }
+
+  function openDialog () {
+    open().then(files => console.log(files))
+  }
+
+  function emitMessage() {
+    emit('front-to-back', "hello from front")
+  }
+
+  useEffect(() => {
+    let unlisten: any;
+    async function f() {
+      unlisten = await listen('back-to-front', event => {
+        console.log(`back-to-front ${event.payload} ${new Date()}`)
+      });
+    }
+    f();
+
+    return () => {
+      if (unlisten) {
+        unlisten();
+      }
+    }
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
+
+          <br></br>
+          Hello Tauri
         </p>
+        <button onClick={executeCommands}>Click to execute command</button>
+        <br></br>
+        <button onClick={openDialog}>Click to open dialog</button>
+        <br></br>
+        <button onClick={emitMessage}>Click to emit message</button>
         <a
           className="App-link"
           href="https://reactjs.org"
